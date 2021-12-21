952b473813fea7fb104120647c0cea6babb343ab
generationokd <generation.o.k.d.1109@gmail.com>
2021-12-21 19:35:43 +0900

    practice

---
+++ src/Counter.tsx
@@ -0,0 +1,49 @@
+import React,{useEffect,useRef,useState} from 'react';
+
+const Counter: React.FC<{}> = () => {
+    const initialValue:any=0;
+    const [value, setValue] = useState<number>(initialValue);
+
+    // const increment = () => {
+    //     setValue(value + 1);
+    // }
+    // const decrement = () => {
+    //     setValue(value - 1);
+    // }
+    const increment = () => {
+        setValue((prevState) => prevState + 1);
+    }
+    const decrement = () => {
+        setValue((prevState) => prevState - 1);
+    }
+
+    const renderTimes = useRef<number>(0);
+    useEffect(()=>{
+        renderTimes.current = renderTimes.current + 1;
+    });
+
+    const ref = useRef<HTMLInputElement>(null!);
+    const focusInput = () => {
+        // const current = ref.current;
+        // if (current != null) current.focus();
+
+        // nullの時は?以降は実行しない
+        // ref.current?.focus();
+        // 初期値null!にすると、?を付けなくてもエラーがでない(refがnullでないと言っているから)
+        ref.current.focus();
+
+    }
+
+    return (
+        <div>
+            <div>value: {value}</div>
+            <button onClick={increment}>+1</button>
+            <button onClick={decrement}>-1</button>
+            <div>This component was re-rendered {renderTimes.current}times!</div>
+            <input ref={ref} type="text" />
+            <button onClick={focusInput}>Click Me!</button>
+        </div>
+    );
+}
+
+export default Counter;
